a7b5a80e9c504f12226157b3744fee75d4a4f832
Abhilash Kumar <abhi.iitk16@gmail.com>
2015-03-24 18:04:30 +0530

    tac.py added

---
+++ src/tac.py
@@ -0,0 +1,61 @@
+#Code stores array of all three address codes
+code = {'program':[]}
+# quad = {'program':-1}
+# nextQuad = {"program":0}
+
+tempVarBaseName = "var"
+varCount = 0
+
+def getNewTempVar():
+	global varCount
+	varCount += 1
+	return tempVarBaseName + str(varCount)
+
+
+# def incrementQuad(functionName):
+# 	global quad
+# 	quad[functionName] = quad[functionName] + 1
+# 	return quad[functionName]
+
+def getNextQuad(functionName):
+	# return quad[functionName] + 1
+	return len(code[functionName])
+
+def getCodeLength(functionName):
+	return len(code[functionName]) - 1
+
+def emitCode(functionName, regDest, regSrc1, regSrc2, op):
+	global code
+	code[functionName].append([regDest, regSrc1, regSrc2, op])
+	# incrementQuad(functionName)
+
+def createNewFucntionCode(functionName):
+	global code , quad
+	# quad[functionName] = -1
+	code[functionName] = []
+
+def printcode():
+	for functionName in code.keys():
+		print "\n",functionName,":"
+		for i in range(len(code[functionName])):
+			print  "%5d: \t" %i, code[functionName][i]
+
+def merge(list1, list2):
+	return list1+list2
+
+def backPatch(functionName, locationList, location):
+	global code
+	for position in locationList:
+		code[functionName][position][2] = location
+
+def noop(functionName, locationList):
+	global code
+	for position in locationList:
+		code[functionName][position][3] = 'NOOP'
+
+
+
+
+	
+
+
